706d8ed72a9cef306158f89e86628d37403d2a43
Jacob Adam Nevitt <jacobadamnevitt@Jacobs-MacBook-Pro.local>
2019-08-22 21:34:00 +0100

    additional info added for hosting

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,9 @@
 const knex = require("knex");
-const dbConfig = require('../knexfile');
-const connection = knex(dbConfig);
+const ENV = process.env.NODE_ENV || "development";
 
-module.exports = connection;
+const dbConfig =
+  ENV === "production"
+    ? { client: "pg", connection: process.env.DATABASE_URL }
+    : require("../knexfile");
+
+module.exports = knex(dbConfig);
